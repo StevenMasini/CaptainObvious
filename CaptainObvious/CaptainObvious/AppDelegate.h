5a350192c73847f68aa15703496956f39d71db57
Steven Masini <steven.masini@curiositychina.com>
2016-05-18 10:07:27 +0800

    Initial Commit.

---
+++ CaptainObvious/CaptainObvious/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaptainObvious
+//
+//  Created by Steven Masini on 5/18/16.
+//  Copyright © 2016 Steven Masini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaptainObvious/CaptainObvious/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptainObvious
+//
+//  Created by Steven Masini on 5/18/16.
+//  Copyright © 2016 Steven Masini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
